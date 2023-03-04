9148e40c1ffccbc18e9e3ace8ff1255c6e336ee1
Serhii Sydorenko <sidorchik91@gmail.com>
2023-03-04 14:46:40 +0200

    update on the first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,8 +14,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      {/* <UserProfile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} /> */}
-      
+      <UserProfile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+
     </div>
   );
 };
